3b5bf2f2a756b13eaac0a1bea64c6b07199bc2dc
Alexander Nitsche <typo3@alexandernitsche.com>
2020-10-14 11:42:36 +0200

    [BUGFIX] Add missing Export::process
    This change adds additional headers to all export files.
    Mock Export::setMetaData() in all file comparison tests as it adds
    the changing timestamp to the export file.
    Add Environment::getPublicPath() to all file paths in testing
    environments.

--- typo3/sysext/impexp/Tests/Functional/Command/ExportCommandTest.php
+++ typo3/sysext/impexp/Tests/Functional/Command/ExportCommandTest.php
@@ -15,9 +15,13 @@
 
 namespace TYPO3\CMS\Impexp\Tests\Functional\Command;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use Symfony\Component\Console\Tester\CommandTester;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Impexp\Command\ExportCommand;
+use TYPO3\CMS\Impexp\Export;
 use TYPO3\CMS\Impexp\Tests\Functional\AbstractImportExportTestCase;
+use TYPO3\TestingFramework\Core\AccessibleObjectInterface;
 
 /**
  * Test case
@@ -25,19 +29,35 @@ use TYPO3\CMS\Impexp\Tests\Functional\AbstractImportExportTestCase;
 class ExportCommandTest extends AbstractImportExportTestCase
 {
     /**
+     * @var Export|MockObject|AccessibleObjectInterface
+     */
+    protected $exportMock;
+
+    /**
+     * @var ExportCommand|MockObject|AccessibleObjectInterface
+     */
+    protected $commandMock;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->exportMock = $this->getAccessibleMock(Export::class, ['setMetaData']);
+        $this->commandMock = $this->getAccessibleMock(ExportCommand::class, ['getExport']);
+        $this->commandMock->expects(self::any())->method('getExport')->willReturn($this->exportMock);
+    }
+
+    /**
      * @test
      */
     public function exportCommandSavesExportWithGivenFileName(): void
     {
         $fileName = 'empty_export.xml';
 
-        /** @var ExportCommand */
-        $command = new ExportCommand();
-        $tester = new CommandTester($command);
+        $tester = new CommandTester($this->commandMock);
         $tester->execute(['file' => $fileName], []);
 
         preg_match('/([^\s]*importexport[^\s]*)/', $tester->getDisplay(), $display);
-        $filePath = $display[1];
+        $filePath = Environment::getPublicPath() . '/' . $display[1];
 
         self::assertEquals(0, $tester->getStatusCode());
         self::assertStringEndsWith('empty_export.xml', $filePath);
