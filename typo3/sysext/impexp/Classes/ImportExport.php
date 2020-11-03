07e9abd5819de3d528f9f44495f7813a69d0a553
Alexander Nitsche <typo3@alexandernitsche.com>
2020-11-03 15:47:51 +0100

    [TASK] Refactor error logging
    Replace ugly logging per debug rendering by
    simple one-error-per-line-logging.
    Add exception message to error log.

--- typo3/sysext/impexp/Classes/ImportExport.php
+++ typo3/sysext/impexp/Classes/ImportExport.php
@@ -1245,16 +1245,6 @@ abstract class ImportExport
     }
 
     /**
-     * Returns a table with the error-messages.
-     *
-     * @return string HTML print of error log
-     */
-    public function printErrorLog()
-    {
-        return !empty($this->errorLog) ? DebugUtility::viewArray($this->errorLog) : '';
-    }
-
-    /**
      * @return BackendUserAuthentication
      */
     protected function getBackendUser()
