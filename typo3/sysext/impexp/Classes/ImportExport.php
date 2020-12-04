cc5e2590e109fd5252adfd61b6eec46ee6180f79
Alexander Nitsche <typo3@alexandernitsche.com>
2020-12-04 13:11:03 +0100

    [TASK] Reduce code line length of ImportExport::isTableStatic()

--- typo3/sysext/impexp/Classes/ImportExport.php
+++ typo3/sysext/impexp/Classes/ImportExport.php
@@ -1165,7 +1165,9 @@ abstract class ImportExport
     protected function isTableStatic(string $table): bool
     {
         if (is_array($GLOBALS['TCA'][$table])) {
-            return ($GLOBALS['TCA'][$table]['ctrl']['is_static'] ?? false) || in_array($table, $this->relStaticTables) || in_array('_ALL', $this->relStaticTables);
+            return ($GLOBALS['TCA'][$table]['ctrl']['is_static'] ?? false)
+                || in_array($table, $this->relStaticTables)
+                || in_array('_ALL', $this->relStaticTables);
         }
         return false;
     }
