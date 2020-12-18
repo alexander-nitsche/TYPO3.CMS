f12941a9b2f9f8de1fdbc7ae2bf03e26b544583b
Alexander Nitsche <typo3@alexandernitsche.com>
2020-12-18 10:37:18 +0100

    [TASK] Refactor storage assignment of sys_file import
    Add functional test.

---
+++ typo3/sysext/impexp/Tests/Functional/Import/ImagesWithStoragesTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Impexp\Tests\Functional\Import;
+
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Impexp\Import;
+use TYPO3\CMS\Impexp\Tests\Functional\AbstractImportExportTestCase;
+
+/**
+ * Test case
+ */
+class ImagesWithStoragesTest extends AbstractImportExportTestCase
+{
+    /**
+     * @test
+     */
+    public function importMultipleImagesWithMultipleStorages(): void
+    {
+        GeneralUtility::mkdir(Environment::getPublicPath() . '/fileadmin-1');
+        GeneralUtility::mkdir(Environment::getPublicPath() . '/fileadmin-3');
+
+        $subject = GeneralUtility::makeInstance(Import::class);
+        $subject->setPid(0);
+        $subject->loadFile(
+            'EXT:impexp/Tests/Functional/Fixtures/XmlImports/images-with-storages.xml',
+            true
+        );
+        $subject->importData();
+
+        $this->assertCSVDataSet('EXT:impexp/Tests/Functional/Fixtures/DatabaseAssertions/importImagesWithStorages.csv');
+        self::assertFileExists(Environment::getPublicPath() . '/fileadmin-1/user_upload/typo3_image3.jpg');
+        self::assertFileExists(Environment::getPublicPath() . '/fileadmin-3/user_upload/typo3_image2.jpg');
+    }
+}
